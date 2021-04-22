d1ac06f2ac6a1cdd2bac9b924073e35d3c82609b
rasvv <rasvv@mail.ru>
2021-04-22 16:38:44 +0300

    Сделал разделение форм

--- src/load.js
+++ src/load.js
@@ -1,12 +1,10 @@
 const loadScript = (src, callback) => {
 	if (src != null) {
-    const el = document.createElement('script');
-    el.src = src;
-    el.type = 'text/javascript';
-    el.onload = callback;
-
-    document.head.appendChild(el);
+    	const el = document.createElement('script');
+    	el.src = src;
+    	el.type = 'text/javascript';
+    	document.body.appendChild(el); 
+	 	el.onload = callback;
+		document.body.insertAdjacentHTML('beforeend', `<div>.</div>`)
 	}
 };
-
-
